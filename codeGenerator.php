c6f1b4fb3233d1db9bbdf5f5b2208adef86acd53
Ramin Rezaei <marssilen5@gmail.com>
2021-09-29 19:17:35 +0330

    بررسی صحت کد ملی
    بررسی صحت کد ملی ایرانیان

--- codeGenerator.php
+++ codeGenerator.php
@@ -16,4 +16,21 @@ function nationalCodeGenerator()
     }
     return $nationalCode;
 }
+
+ private function checkNationalCode($code)
+    {
+        if (!preg_match('/^[0-9]{10}$/', $code))
+            return false;
+        for ($i = 0; $i < 10; $i++)
+            if (preg_match('/^' . $i . '{10}$/', $code))
+                return false;
+        for ($i = 0, $sum = 0; $i < 9; $i++)
+            $sum += ((10 - $i) * intval(substr($code, $i, 1)));
+        $ret = $sum % 11;
+        $parity = intval(substr($code, 9, 1));
+        if (($ret < 2 && $ret == $parity) || ($ret >= 2 && $ret == 11 - $parity))
+            return true;
+        return false;
+    }
+
 echo nationalCodeGenerator();
